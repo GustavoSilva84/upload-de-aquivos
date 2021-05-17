1343fb1c637759986dd89045346046222df46528
Gustavo-Silva-01 <fogofracoo@gmail.com>
2021-05-17 10:15:34 -0300

    iniciado servidor

--- src/server.js
+++ src/server.js
@@ -1,3 +1,8 @@
 import app from './app.js'
 
-app.listen()
+app.listen(process.env.PORT || 8181, (erro) => {
+
+    if(erro) return console.log(`Erro: ${erro}`);
+    console.log(`Servidor iniciado`);
+
+});
